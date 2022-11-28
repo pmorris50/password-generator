722963cb25357fa82189dea0712423f54b8f665f
pmorris50 <patrickmorris532@gmail.com>
2022-11-27 17:34:56 -0700

    prompting user input

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
 // Assignment Code
 //Defrining variables for password criteria
-var upperCase = ['a', 'b', 'c', 'd']
 //prompt user for password criteria
 
 // Length of password has to be at least 8 characters and no more than 128 characters
@@ -12,9 +11,35 @@ var upperCase = ['a', 'b', 'c', 'd']
 //1. Prompt user to determine pass length (must be between 8-128 characters ) 
 // 2. create function to prompt user for password options
     //a. confirm - Would you like to include lower case, uupper case, special, numbers. 4 con
-// 3. 
 
 
+//BLOCK
+//code for lower case letter array where each letter is separate string, var = lcArray
+
+var alphabet = ["a, b, c, d, e, f, g, h, i, j ,k , l, m, n, o, p, q, r, s, t, u, v, x, y, z"]
+// console.log(alphabet);
+var lcArray =alphabet.toString().split(",") //This is my lowercase letter array 
+console.log(lcArray)
+// var inclLowerCase = confirm('Would you like to include lower case letters.'); //prompt user to include lower case letters
+
+// var for Upper Case Array. Use upCaseArray for for loop
+var upCaseArray = alphabet.toString().toUpperCase().split(",");
+console.log(upCaseArray);
+//BlOCK
+
+//array for special characters
+var specChar = [
+  "!, @, #, $, %, ^, &, *, (, ), -, _, =, +, `, ~, <, ., >, ?, :, ;, [, {, ], }, ,"
+]
+var specChar1 = specChar.toString().split(','); //variable to use for forloop
+console.log(specChar1); 
+//Block
+
+//array for numbers
+//BLOCK
+var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']; //array for number character
+console.log(num);
+//BLOCK
 
 
 
@@ -36,34 +61,16 @@ function writePassword(
 }
 
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+var promptuser = function(){
+  prompt('How many characters do you want your password to be in? ');
+  confirm('Would you like to include lower case letters in your password?');
+  confirm('Would you like to include upper case letters in your password?');
+  confirm('Would you like to include special characters in your password?');
+  confirm('Would you like to include numbers in your password?')
+}
 
-//BLOCK
-//code for lower case letter array where each letter is separate string, var = lcArray
-var alphabet = ["a, b, c, d, e, f, g, h, i, j ,k , l, m, n, o, p, q, r, s, t, u, v, x, y, z"]
-// console.log(alphabet);
-var lcArray =alphabet.toString().split(",") //This is my lowercase letter array 
-console.log(lcArray)
-var inclLowerCase = confirm('Would you like to include lower case letters.'); //prompt user to include lower case letters
-
-// var for Upper Case Array. Use upCaseArray for for loop
-var upCaseArray = alphabet.toString().toUpperCase().split(",");
-console.log(upCaseArray);
-//BlOCK
-
-//array for special characters
-var specChar = [
-  "!, @, #, $, %, ^, &, *, (, ), -, _, =, +, `, ~, <, ., >, ?, :, ;, [, {, ], }, ,"
-]
-var specChar1 = specChar.toString().split(','); //variable to use for forloop
-console.log(specChar1); 
-//Block
-
-//array for numbers
-//BLOCK
-var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']; //array for number character
-console.log(num);
-//BLOCK
-
+promptuser();
