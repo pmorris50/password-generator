31ed9183641a80f61e304c89d5d9dd109758a80f
pmorris50 <patrickmorris532@gmail.com>
2022-11-28 18:04:18 -0700

    if statements to concat arrays completed, need to create for loop to grab data from concat arrays

--- assets/script.js
+++ assets/script.js
@@ -10,103 +10,153 @@
 // My Psuedo Code
 //1. Prompt user to determine pass length (must be between 8-128 characters ) 
 // 2. create function to prompt user for password options
-    //a. confirm - Would you like to include lower case, uupper case, special, numbers. 4 con
+//a. confirm - Would you like to include lower case, uupper case, special, numbers. 4 con
 
 
-//BLOCK
-//code for lower case letter array where each letter is separate string, var = lcArray
 
-// var alphabet = ["a, b, c, d, e, f, g, h, i, j ,k , l, m, n, o, p, q, r, s, t, u, v, x, y, z"]
-// // console.log(alphabet);
-// var lcArray =alphabet.toString().split(",") //This is my lowercase letter array 
-// console.log(lcArray)
-// // var inclLowerCase = confirm('Would you like to include lower case letters.'); //prompt user to include lower case letters
 
-// // var for Upper Case Array. Use upCaseArray for for loop
-// var upCaseArray = alphabet.toString().toUpperCase().split(",");
-// console.log(upCaseArray);
-// //BlOCK
+var alphabet = ["a, b, c, d, e, f, g, h, i, j ,k , l, m, n, o, p, q, r, s, t, u, v, x, y, z"]
+  // console.log(alphabet);
+var lcArray =alphabet.toString().split(",") //This is my lowercase letter array 
+//console.log(lcArray)
 
-// //array for special characters
-// var specChar = [
-//   "!, @, #, $, %, ^, &, *, (, ), -, _, =, +, `, ~, <, ., >, ?, :, ;, [, {, ], }, ,"
-// ]
-// var specChar1 = specChar.toString().split(','); //variable to use for forloop
+
+// var for Upper Case Array. Use upCaseArray for for loop
+var upCaseArray = alphabet.toString().toUpperCase().split(",");
+//console.log(upCaseArray);
+
+
+ //array for special characters
+var specChar = [
+  "!, @, #, $, %, ^, &, *, (, ), -, _, =, +, `, ~, <, ., >, ?, :, ;, [, {, ], }, ,"
+]
+var specCharArray = specChar.toString().split(','); 
 // console.log(specChar1); 
-// //Block
 
-// //array for numbers
-// //BLOCK
-// var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']; //array for number character
+
+//array for numbers
+var numberArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']; 
 // console.log(num);
-// //BLOCK
-var promptUser = function(){
+
+
+function userOptions () {
 
   var length = parseInt(prompt('How many characters do you want your password to be? '));
   if (Number.isNaN(length)) {
     alert('Password must be a numeric number');
     return null;
-  } 
-
-  if (length > 128 || length < 8 ){
-    alert('Password must be a numeric number between 8-128');
-   return null;
   }
 
-  console.log(length);
+  if (length > 128 || length < 8) {
+    alert('Password must be a numeric number between 8-128');
+    return null;
+  }
+
+//  console.log(length);
   var incLowerCase = confirm('Would you like to include lower case letters in your password?');
   var incUpperCase = confirm('Would you like to include upper case letters in your password?');
   var incSpecial = confirm('Would you like to include special characters in your password?');
-  var incNumbers = confirm('Would you like to include numbers in your password?')
+  var incNumbers = confirm('Would you like to include numbers in your password?');
 
 
 
-  console.log(incLowerCase);
-  console.log(incUpperCase);
-  console.log(incSpecial);
-  console.log(incNumbers);
+  // console.log(incLowerCase);
+  // console.log(incUpperCase);
+  // console.log(incSpecial);
+  // console.log(incNumbers);
 
-if(
-  incLowerCase ===false &&
-  incNumbers === false &&
-  incSpecial === false &&
-  incUpperCase === false 
-){
-alert("You must confirm at least 1 character option");
+  if (
+    incLowerCase === false &&
+    incNumbers === false &&
+    incSpecial === false &&
+    incUpperCase === false
+  ) {
+    alert("You must confirm at least 1 character option");
 
-//this is the return if a condition is not met returning null ends the function
-return null;
-}
-//object that stores users inputs
-var passwordCriteria = {
-  length: length,
-  incLowerCase: incLowerCase,
-  incNumbers: incNumbers,
-  incUpperCase: incUpperCase,
-  incSpecial: incSpecial
-};
-return passwordCriteria
-}
-
+    //this is the return if a condition is not met returning null ends the function
+    return null;
+  }
+  //object that stores users inputs
+  var passwordCriteria = {
+    length: length,
+    incLowerCase: incLowerCase,
+    incNumbers: incNumbers,
+    incUpperCase: incUpperCase,
+    incSpecial: incSpecial
+    
+  };
+  // console.log(passwordCriteria);
+  // return passwordCriteria;
+  
 
 // If something happens return this elsie if this is met return this
 
-//function for fanomizing elements from an array
-function randomArray(arr)
+//function to randomize array data
+
+
+};
+//functino to shuffle array order. replace arr with array name
+function shufArray(arr){
+  var randomInteger = Math.floor(Math.random() *arr.length);
+  var randomCharacter = arr[randomInteger];
+  return randomCharacter;
+}
+
+
+function generatePassword(){
+var userChoice = userOptions();
+var acceptedCharacters = [];
+var guaranteeCharacters =[];
+var results = [];
+
+//if user did not select do nothing
+if(!userChoice) 
+return null;
+
+if(userChoice.incLowerCase){
+  acceptedCharacters = acceptedCharacters.concat(lcArray);
+  guaranteeCharacters.push(shufArray(lcArray));
+ 
+
+
+ };
+ if(userChoice.incUpperCase){
+  acceptedCharacters = acceptedCharacters.concat(upCaseArray)
+  guaranteeCharacters.push(shufArray(upCaseArray));
+  
+};
+if(userChoice.incNumbers){
+  acceptedCharacters = acceptedCharacters.concat(numberArray);
+  guaranteeCharacters.push(shufArray(numberArray));
+}
+if(userChoice.incSpecial){
+  acceptedCharacters = acceptedCharacters.concat(specCharArray);
+  guaranteeCharacters.push(shufArray(specCharArray));
+}
+
+//return(guaranteeCharacters);
+for (var i = 0; i < length; i++){
+    randomInteger
 
 
 
-
-
-
+}
+console.log(randomInteger);
+}
+// console.log(generatePassword());
 
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword(
-  
-) { promptUser();
-  //MY CODE GOES HERE
+function writePassword() {
+ 
+
+
+
+
+
+ 
+ 
 
 
   var password = generatePassword();
